96e9a59b209f0534da6964097ad601ca135e08a0
chenrui0704 <577379328@qq.com>
2021-03-03 15:04:38 +0800

    no message

--- src/main/resources/model/${table.classNameFirstLower}/${className}.java
+++ src/main/resources/model/${table.classNameFirstLower}/${className}.java
@@ -1,44 +1,15 @@
 package ${package}.entity;
 
 import java.util.Date;
+import lombok.Data;
+
+@Data
 public class ${className} {
 
 <#list table.columns as column>
 <#assign length ="${column.javaType?length}">
 <#assign end ="${column.javaType?last_index_of(\".\")}">
-	private ${column.javaType?substring("${end}"?number+1,"${length}"?number)} ${column.columnNameLower};
+	private ${column.javaType?substring("${end}"?number+1,"${length}"?number)} ${column.columnNameLower}; // ${column.columnAlias}
 </#list>
-
-<#list table.columns as column>
-<#assign length ="${column.javaType?length}">
-<#assign end ="${column.javaType?last_index_of(\".\")}">
-	public void set${column.columnName}(${column.javaType?substring("${end}"?number+1,"${length}"?number)} value) {
-		this.${column.columnNameLower} = value;
-	}
-
-	public ${column.javaType?substring("${end}"?number+1,"${length}"?number)} get${column.columnName}() {
-		return this.${column.columnNameLower};
-	}
-</#list>
-
-
-	public ${className} (){
-
-	}
-
-	public ${className} (
-			<#list table.columns as column>
-<#assign length ="${column.javaType?length}">
-<#assign end ="${column.javaType?last_index_of(\".\")}">
- ${column.javaType?substring("${end}"?number+1,"${length}"?number)} ${column.columnNameLower} <#if column_has_next>,</#if>
-</#list>
-		){
-	<#list table.columns as column>
-	<#assign length ="${column.javaType?length}">
-	<#assign end ="${column.javaType?last_index_of(\".\")}">
-		this.${column.columnNameLower} = ${column.columnNameLower};
-	</#list>
-	}
-
 
 }
